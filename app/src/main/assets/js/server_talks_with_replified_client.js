57e62bb3ecb7b5b9dc058ffb26c69245f7e830af
Jasm Sison <jasm.sison@gmail.com>
2015-02-24 11:51:10 +0100

    added script to start repl on the socket client

--- app/src/main/assets/js/server_talks_with_replified_client.js
+++ app/src/main/assets/js/server_talks_with_replified_client.js
@@ -1,3 +1,4 @@
+// abstract server
 var net  = require('net'),
     repl = require('repl');
 
@@ -24,10 +25,35 @@ var socket = net.createConnection({path:socketPath}, function () {
   });
 });
 
-//var socketPath = '/home/me/server-replified-client.sock';
-//var socketPath = '/data/data/nl.sison.android.nodejs.repl/cache/node-repl-x.sock';
+// fs server
+var net  = require('net'),
+    repl = require('repl');
 
-// abstract #0
+var socketPath = '/data/data/nl.sison.android.nodejs.repl/cache/mehrepl.sock';
+
+var server = net.createServer(function(socket) {
+  console.log('client connected');
+
+  socket.on('data', function(data) {
+    console.log(data.toString('UTF-8'));
+  });
+
+});
+
+server.listen(socketPath);
+
+var socket = net.createConnection({path:socketPath}, function () {
+  repl.start({
+    prompt: "node via Unix socket xxxxxxx> ",
+    input: socket,
+    output: socket
+  }).on('exit', function() {
+    socket.end();
+  });
+});
+
+
+// abstract client #0
 var socket = net.createConnection({path:"\\0abstract"}, function () {
   repl.start({
     prompt: "node via Unix socket xxxxxxx> ",
@@ -38,7 +64,7 @@ var socket = net.createConnection({path:"\\0abstract"}, function () {
   });
 });
 
-// abstract #1
+// abstract client #1
 var socket = net.createConnection({path:"\0abstract"}, function () {
   repl.start({
     prompt: "node via Unix socket xxxxxxx> ",
@@ -49,7 +75,7 @@ var socket = net.createConnection({path:"\0abstract"}, function () {
   });
 });
 
-// fs #0
+// fs client #0
 var socket = net.createConnection({path:'/data/data/nl.sison.android.nodejs.repl/cache/repl.sock'}, function () {
   repl.start({
     prompt: "node via Unix socket xxxxxxx> ",
